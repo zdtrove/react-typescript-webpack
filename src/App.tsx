0d3a9d018d2624addb1f8cf918b75735993c9e75
Hoang An <antnh@icd-vn.com>
2021-03-25 08:56:19 +0700

    cm

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import LOGO from './logo.svg'
 
 export const App = () => {
     return <>
-        <h1>React Typescript Webpack</h1>
+        <h1>React Typescript Webpack - {process.env.NODE_ENV} - {process.env.name}</h1>
         <img src={IMAGE} alt="React logo" width="300" />
         <img src={LOGO} alt="React logo" width="300" />
     </>
